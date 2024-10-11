fb3822a9ac205243707e5a9e2d0ceb76e12e8f0d
sophia <sofiya.brovkina@inbox.ru>
2024-10-11 19:17:03 +0300

    add fourth lab

--- mainPage/scripts/AddLineScript.js
+++ mainPage/scripts/AddLineScript.js
@@ -1,4 +1,3 @@
-
 let rowCount = 3;
 const addLineButton = document.querySelector('.addLine');
 const jobTableBody = document.getElementById('jobTableBody');
@@ -6,13 +5,17 @@ const jobTableBody = document.getElementById('jobTableBody');
 addLineButton.addEventListener('click', function() {
     const newRow = document.createElement('tr');
     newRow.className = rowCount % 2 === 0 ? 'even' : 'odd';
-    newRow.innerHTML = `
-            <td>${rowCount}</td>
-            <td contenteditable="true"></td>
-            <td contenteditable="true"></td>
-            <td contenteditable="true"></td>
-            <td contenteditable="true"></td>
-            <td contenteditable="true"></td>`;
-   jobTableBody.appendChild(newRow);
-   rowCount++;
+
+    const numberCell = document.createElement('td');
+    numberCell.textContent = rowCount;
+    newRow.appendChild(numberCell);
+
+    for (let i = 1; i <= 5; i++) {
+        const editableCell = document.createElement('td');
+        editableCell.contentEditable = "true";
+        newRow.appendChild(editableCell);
+    }
+
+    jobTableBody.appendChild(newRow);
+    rowCount++;
 });
