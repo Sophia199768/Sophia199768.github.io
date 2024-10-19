fa440bd15ff1d3fe95c0ea8a46ce808f29729a69
sophia <sofiya.brovkina@inbox.ru>
2024-10-19 13:00:53 +0300

    add fifth lab

--- mainPage/scripts/AllPagesScript.js
+++ mainPage/scripts/AllPagesScript.js
@@ -1,3 +1,6 @@
+let numberOfTopics = 1;
+let imageElement
+
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.querySelector('.theme-toggle-button');
 
@@ -5,9 +8,11 @@ document.addEventListener('DOMContentLoaded', () => {
         if (theme === 'blue') {
             document.body.classList.add('blue-theme');
             document.body.classList.remove('light-theme');
+            numberOfTopics = 1;
         } else {
             document.body.classList.add('light-theme');
             document.body.classList.remove('blue-theme');
+            numberOfTopics = 2;
         }
     }
 
@@ -19,5 +24,14 @@ document.addEventListener('DOMContentLoaded', () => {
         const newTheme = currentTheme === 'blue' ? 'light' : 'blue';
         setTheme(newTheme);
         localStorage.setItem('theme', newTheme);
+
+        imageElement = document.getElementById('learningImage');
+
+        if (numberOfTopics === 1) {
+            imageElement.src = 'pictures/Creative.png';
+        } else {
+            imageElement.src = 'pictures/Learning.png';
+        }
     });
 });
+
