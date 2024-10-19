ffb87769f8916eafaf6ef549800d13efc967fb99
sophia <sofiya.brovkina@inbox.ru>
2024-10-19 13:36:22 +0300

    update fifth lab

--- mainPage/scripts/Iife.js
+++ mainPage/scripts/Iife.js
@@ -4,5 +4,12 @@ window.onload = loadTime;
 
 function loadTime() {
     let loadInSeconds = (new Date().getTime() - time) / 1000;
-    document.getElementById("load_time").innerHTML = "Load time is <b>" + loadInSeconds + "</b> seconds";
+
+    let loadTimeElement = document.getElementById("load_time");
+    let textNode = document.createTextNode("Load time is ");
+    let style = document.createElement("p");
+    style.textContent = loadInSeconds;
+    loadTimeElement.textContent = "";
+    loadTimeElement.appendChild(textNode);
+    loadTimeElement.appendChild(style);
 }
